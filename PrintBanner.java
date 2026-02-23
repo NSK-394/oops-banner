ec60e1b069db6fbde02d4c2a2e7c0f5aafa569de
K NIKHIL SAI <ks8274@srmist.edu.in>
2026-02-23 13:57:56 +0530

    uc4

--- PrintBanner.java
+++ PrintBanner.java
@@ -1,13 +1,15 @@
 public class PrintBanner {
+    public static void print() {
+        System.out.println("  *****    *****   *****   *****  ");
+        System.out.println(" *     *  *     *  *    * *     * ");
+        System.out.println(" *     *  *     *  *    * *       ");
+        System.out.println(" *     *  *     *  *****   *****  ");
+        System.out.println(" *     *  *     *  *            * ");
+        System.out.println(" *     *  *     *  *      *     * ");
+        System.out.println("  *****    *****   *       *****  ");
+    }
+
     public static void main(String[] args) {
-
-        System.out.println(" *****   *****   ******   ***** ");
-        System.out.println("*     * *     *  *     * *     *");
-        System.out.println("*     * *     *  *     * *      ");
-        System.out.println("*     * *     *  ******   ***** ");
-        System.out.println("*     * *     *  *             *");
-        System.out.println("*     * *     *  *       *     *");
-        System.out.println(" *****   *****   *        ***** ");
-
+        print();
     }
 }
